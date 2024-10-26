f52c848381b0868ed9cf85482f3279e10d816de5
Aqil Reza <aqilreza651@gmail.com>
2024-10-26 14:45:14 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import PocketBase from "pocketbase";
+
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+const titleVal = ref("");
+const data = ref([]);
+
+async function Submit() {
+
+// example create data
+const record = {
+    title: titleVal.value,
+    description: "desc",
+};
+
+  await pb.collection('posts').create(record);
+
+  data.value.push(record);
+  }
+
+onMounted(async() => {
+  const result = await pb.collection('posts').getFullList();
+
+  data.value = result;
+})
+</script>
+
+<template>
+<input class="bg-stone-700" v-model="titleVal">
+<button @click="Submit">Add</button>
+<button class="bg-pink-200 text-white ">hello</button>
+
+</template>
